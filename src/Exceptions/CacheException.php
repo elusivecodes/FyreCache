f7fe14b638274439f075c01baf7517319adad7ce
Elusive <elusivecodes@gmail.com>
2022-04-04 17:38:26 +1000

    - Modified CacheException to extend Exception.

--- src/Exceptions/CacheException.php
+++ src/Exceptions/CacheException.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 namespace Fyre\Cache\Exceptions;
 
 use
-    RunTimeException;
+    Fyre\Error\Exceptions\Exception;
 
 /**
  * CacheException
  */
-class CacheException extends RunTimeException
+class CacheException extends Exception
 {
 
     public static function forAuthFailed()
