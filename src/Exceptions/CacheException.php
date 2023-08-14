4145d5069cfcf3e2246b82e51db8d10e9c2fbc74
Elusive <18050480+elusivecodes@users.noreply.github.com>
2023-08-14 12:49:12 +1000

    - Added return types to CacheException methods.

--- src/Exceptions/CacheException.php
+++ src/Exceptions/CacheException.php
@@ -11,42 +11,42 @@ use RuntimeException;
 class CacheException extends RuntimeException
 {
 
-    public static function forAuthFailed()
+    public static function forAuthFailed(): static
     {
         return new static('Cache handler authentication failed');
     }
 
-    public static function forConfigExists(string $key)
+    public static function forConfigExists(string $key): static
     {
         return new static('Cache handler config already exists: '.$key);
     }
 
-    public static function forConnectionError(string $message = '')
+    public static function forConnectionError(string $message = ''): static
     {
         return new static('Cache handler connection error: '.$message);
     }
 
-    public static function forConnectionFailed()
+    public static function forConnectionFailed(): static
     {
         return new static('Cache handler connection failed');
     }
 
-    public static function forInvalidClass(string $className = '')
+    public static function forInvalidClass(string $className = ''): static
     {
         return new static('Cache handler class not found: '.$className);
     }
 
-    public static function forInvalidConfig(string $key)
+    public static function forInvalidConfig(string $key): static
     {
         return new static('Cache handler invalid config: '.$key);
     }
 
-    public static function forInvalidDatabase(string $database)
+    public static function forInvalidDatabase(string $database): static
     {
         return new static('Cache handler invalid database: '.$database);
     }
 
-    public static function forInvalidKey(string $key)
+    public static function forInvalidKey(string $key): static
     {
         return new static('Cache handler invalid key: '.$key);
     }
